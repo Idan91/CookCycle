102611e1a0faba6c143e36a238e030091b49f8ef
Idan91 <idan.ram91@gmail.com>
2021-03-30 18:29:45 +0300

    deleted node_modules

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -19,36 +19,15 @@ export const cookcycleApiCall = async (
         data: requestBody,
       })
         .then((response) => {
-          // console.log(response);
           if (response.status >= 200 && response.status < 300) {
             resolve({ data: response.data, status: response.status });
           } else if (response.status === 401) {
-            // console.error(
-            //   `Error 401, apiCommand: ${apiCommand} ${
-            //     requestBody !== {} &&
-            //     `requestBody: ${JSON.stringify(requestBody)}`
-            //   }`
-            // );
             reject("Unauthenticated");
           } else if (response.status === 500) {
-            // console.log(response);
-
-            // console.error(
-            //   `Error 500, apiCommand: ${apiCommand} ${
-            //     requestBody !== {} &&
-            //     `requestBody: ${JSON.stringify(requestBody)}`
-            //   }`
-            // );
             reject("Server error!");
           }
         })
         .catch((err) => {
-          // console.error(
-          //   `Error, apiCommand: ${apiCommand} ${
-          //     requestBody !== {} &&
-          //     `requestBody: ${JSON.stringify(requestBody)}`
-          //   }`
-          // );
           reject(err);
         });
     } catch (err) {
